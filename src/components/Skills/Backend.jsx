38407c101ff4eb76a05b53b49f8c4fd02e370121
tiaraarista <tiaraarista99@gmail.com>
2023-09-26 08:18:18 +0700

    skills

--- src/components/Skills/Backend.jsx
+++ src/components/Skills/Backend.jsx
@@ -2,7 +2,66 @@ import React from 'react'
 
 const Backend = () => {
   return (
-    <div>Backend</div>
+    
+    <div className="skills__content">
+      <h3 className="skills__title">Backend</h3>
+      <div className="skills__box">
+
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills__name">Laravel</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+          
+          <div className="skills__data">
+            <i class='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills__name">PHP</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills__name">MySQL</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+          
+        </div>
+
+        <div className="skills__group">
+          <div className="skills__data">
+            <i className='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills__name">C#</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+          
+          <div className="skills__data">
+            <i class='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills__name">Java</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class='bx bx-badge-check'></i>
+            <div>
+              <h3 className="skills__name">Flutter</h3>
+              <span className="skills__level">Basics</span>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
   )
 }
 

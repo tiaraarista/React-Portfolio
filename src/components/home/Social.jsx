aaea4c22e06456e07729f16c9c2e53960773af81
tiaraarista <tiaraarista99@gmail.com>
2023-09-27 12:27:53 +0700

    Updated Link

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Social = () => {
   return (
     <div className="home__social">
-        <a href="www.instagram.com" className="home__social-icon" target='_blank'>
+        <a href="https://instagram.com/tiaraarista_" className="home__social-icon" rel="noreferrer" target="_blank">
             <i class="uil uil-instagram"></i>
         </a>
-        <a href="www.github.com" className="home__social-icon" target='_blank'>
+        <a href="https://github.com/tiaraarista" className="home__social-icon" rel="noreferrer" target="_blank">
             <i class="uil uil-github-alt"></i>
         </a>
-        <a href="www.instagram.com" className="home__social-icon" target='_blank'>
-        <i class="uil uil-instagram"></i>
+        <a href="https://twitter.com/tiaraarista_" className="home__social-icon" rel="noreferrer" target="_blank">
+        <i class="uil uil-twitter-alt"></i>
         </a>
     </div>
   )

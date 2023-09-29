922f350ca8f1df5d98b81a3e1e7de887cab21dec
tiaraarista <tiaraarista99@gmail.com>
2023-09-29 22:34:09 +0700

    Modal with SwiperSlide

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -1,39 +1,71 @@
-import AraHelmet from "../../assets/AraHelmet/Halaman Awal.jpg";
-import LaravelBlogSys from "../../assets/LaravelBlogSys/Laravel Blog System.jpg";
-import ARProdi from "../../assets/ARPHB/Home.jpg";
-import Kachujin from "../../assets/KachujinRun/Home.jpg";
-
+import AraHelmetHome from "../../assets/AraHelmet/Halaman Awal.jpg";
+import AraDataBarang from "../../assets/AraHelmet/Data Barang.jpg";
+import AraLapPenjualan from "../../assets/AraHelmet/Laporan Penjualan.jpg";
+import AraTransaksi from "../../assets/AraHelmet/Transaksi.jpg";
+import AraLogin from "../../assets/AraHelmet/Login.jpg";
+import LBSDashboard from "../../assets/LaravelBlogSys/LBSDashboard.jpg";
+import LBSLogin from "../../assets/LaravelBlogSys/LBSLogin.jpg";
+import LBSHome from "../../assets/LaravelBlogSys/LBSHome.jpg";
+import LBSProject from "../../assets/LaravelBlogSys/LBSProject.jpg";
+import LBSCategory from "../../assets/LaravelBlogSys/LBSCategory.jpg";
+import LBSProfile from "../../assets/LaravelBlogSys/LBSProfile.jpg";
+import ARProdiHome from "../../assets/ARPHB/Home.jpg";
+import ARProdi from "../../assets/ARPHB/ARProdi.jpg";
+import ARProdiFasility from "../../assets/ARPHB/Fasility.jpg";
+import ARProdiLoad from "../../assets/ARPHB/Loadscreen.jpg";
+import KachujinHome from "../../assets/KachujinRun/Home.jpg";
+import KachujinPlay from "../../assets/KachujinRun/Play.jpg";
+import KachujinHowtoPlay from "../../assets/KachujinRun/HowToPlay.jpg";
 
 
 
 export const projectsData = [
     {
         id: 1,
-        image : LaravelBlogSys,
+        image : LBSDashboard,
         title: 'Laravel Blog System',
         category: 'web',
         link : 'https://github.com/tiaraarista/laravel-blog-system.git',
+        more1 : LBSDashboard,
+        more2 : LBSDashboard,
+        more3 : LBSLogin,
+        more4 : LBSHome,
+        more5 : LBSProject,
+        more6 : LBSCategory,
+        more7 : LBSProfile,
     },
     {
         id: 2,
-        image : Kachujin,
+        image : KachujinHome,
         title: 'Kachujin Run 3D',
         category: 'game',
         link : 'https://github.com/tiaraarista/kachujin-run.git',
+        more1 : KachujinHome,
+        more2 : KachujinPlay,
+        more3 : KachujinHowtoPlay,
     },
     {
         id: 3,
-        image : ARProdi,
+        image : ARProdiHome,
         title: 'AR PHB',
         category: 'app',
         link : 'https://github.com/tiaraarista/AR-PHB.git',
+        more1 : ARProdiLoad,
+        more2 : ARProdiHome,
+        more3 : ARProdi,
+        more4 : ARProdiFasility,
     },
     {
         id: 4,
-        image : AraHelmet,
+        image : AraHelmetHome,
         title: 'POS System',
         category: 'desktop',
         link : 'https://github.com/tiaraarista/POS-System.git',
+        more1 : AraLogin,
+        more2 : AraHelmetHome,
+        more3 : AraDataBarang,
+        more4 : AraTransaksi,
+        more5 : AraLapPenjualan
     }
 ];
 
--- src/components/portfolio/ProjectItems.jsx
+++ src/components/portfolio/ProjectItems.jsx
@@ -1,13 +1,178 @@
-import React from 'react'
+import React, { useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper/modules';
+
 
 const ProjectItems = ({item}) => {
+  const[toggleState, setTogelState] = useState(0);
+
+    const toggleTab = (id) => {
+        setTogelState(id);
+    }
   return (
     <div className="project__card" key={item.id}>
       <div className="card__header">
         <img src={item.image} alt="" className="project__img" />
       </div>
         <h3 className="project__title">{item.title}</h3>
-        <a href={item.link} className="project__button" rel="noreferrer" target='_blank'>Demo<i className="bx bx-right-arrow-alt project__button-icon"></i></a>
+        <span className="project__button" onClick={()=>toggleTab(item.id)}>View More<i className="uil uil-arrow-right project__button-icon"></i></span>
+        <a href={item.link} className="project__button" rel="noreferrer" target='_blank'>Demo<i className="bx bx-right-arrow-alt project__button-icon"></i></a>   
+
+        <div className={toggleState === 1 ? "services__modal active-modal" : "services__modal"}>
+            <div className="services__modal-content">
+                <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+                <h3 className="services__modal-title">{item.title}</h3>
+                <p className="services__modal-description">
+                    An endless Run Game using Unity C#
+                </p>
+
+                <Swiper loop={true} grabCursor={true} spaceBetween={24} pagination={{ clickable: true, }} breakpoints={{
+                    576: {
+                      slidesPerView: 1,
+                    },
+                    768: {
+                      slidesPerView: 1,
+                      spaceBetween: 48,
+                    },
+                  }} modules={[Pagination]}>
+                    
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more1} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more2} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more3} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more4} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more5} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more6} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more7} alt="" className="project__img" />
+                    </SwiperSlide>
+
+                </Swiper>
+            </div>
+        </div> 
+
+        <div className={toggleState === 2 ? "services__modal active-modal" : "services__modal"}>
+            <div className="services__modal-content">
+                <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+                <h3 className="services__modal-title">{item.title}</h3>
+                <p className="services__modal-description">
+                    An endless Run Game using Unity C#
+                </p>
+
+                <Swiper loop={true} grabCursor={true} spaceBetween={24} pagination={{ clickable: true, }} breakpoints={{
+                    576: {
+                      slidesPerView: 1,
+                    },
+                    768: {
+                      slidesPerView: 1,
+                      spaceBetween: 48,
+                    },
+                  }} modules={[Pagination]}>
+                    
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more1} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more2} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more3} alt="" className="project__img" />
+                    </SwiperSlide>
+
+                </Swiper>
+            </div>
+        </div>     
+
+        <div className={toggleState === 3 ? "services__modal active-modal" : "services__modal"}>
+            <div className="services__modal-content">
+                <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+                <h3 className="services__modal-title">{item.title}</h3>
+                <p className="services__modal-description">
+                    Augmented Reality to Introduce Study Program on PHB using Unity C# and Vuforia Package
+                </p>
+
+                <Swiper loop={true} grabCursor={true} spaceBetween={24} pagination={{ clickable: true, }} breakpoints={{
+                    576: {
+                      slidesPerView: 1,
+                    },
+                    768: {
+                      slidesPerView: 1,
+                      spaceBetween: 48,
+                    },
+                  }} modules={[Pagination]}>
+                    
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more1} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more2} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more3} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more4} alt="" className="project__img" />
+                    </SwiperSlide>
+
+                </Swiper>
+            </div>
+        </div>     
+
+        <div className={toggleState === 4 ? "services__modal active-modal" : "services__modal"}>
+            <div className="services__modal-content">
+                <i onClick={() => toggleTab(0)} className="uil uil-times services__modal-close"></i>
+                <h3 className="services__modal-title">{item.title}</h3>
+                <p className="services__modal-description">
+                    Point of Sales System (Cashier) using Java Netbeans and MySQL
+                </p>
+
+                <Swiper loop={true} grabCursor={true} spaceBetween={24} pagination={{ clickable: true, }} breakpoints={{
+                    576: {
+                      slidesPerView: 1,
+                    },
+                    768: {
+                      slidesPerView: 1,
+                      spaceBetween: 48,
+                    },
+                  }} modules={[Pagination]}>
+                    
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more1} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more2} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more3} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more4} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more5} alt="" className="project__img" />
+                    </SwiperSlide>
+
+                </Swiper>
+            </div>
+        </div>  
     </div>
   );
 }

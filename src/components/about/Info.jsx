056a3dcc35ab0313d3b86829afa81e44f8c83131
tiaraarista <tiaraarista99@gmail.com>
2023-12-18 09:33:04 +0700

    css modif

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -6,21 +6,21 @@ const Info = () => {
         <div className="about__box">
         <i className='bx bx-award about__title'></i>
             <h3 className="about__title">Experiences</h3>
-            <span className="about subtitle">2 Years</span>
+            <span className="about__subtitle">1 Years</span>
         </div>
 
         
         <div className="about__box">
         <i className='bx bx-briefcase  about__title'></i>
             <h3 className="about__title">Completed</h3>
-            <span className="about subtitle">8 + Projects</span>
+            <span className="about__subtitle">8+ Projects</span>
         </div>
 
         
         <div className="about__box">
         <i className='bx bx-support about__title'></i>
             <h3 className="about__title">Support</h3>
-            <span className="about subtitle">24/7</span>
+            <span className="about__subtitle">24/7</span>
         </div>
     </div>
   )
--- src/components/portfolio/ProjectItems.jsx
+++ src/components/portfolio/ProjectItems.jsx
@@ -24,7 +24,7 @@ const ProjectItems = ({item}) => {
         <h3 className="project__title">{item.title}</h3>
         {/* <span className="project__button" onClick={()=>toggleTab(item.id)}>View More<i className="uil uil-arrow-right project__button-icon"></i></span> */}
         {/* <a href={item.link} className="project__button" rel="noreferrer" target='_blank'>Demo<i className="bx bx-right-arrow-alt project__button-icon"></i></a> */}
-        <a href={item.link} className="button button--flex">Demo</a>
+        <a href={item.link} rel="noreferrer" target="_blank" className="button button--flex">Demo</a>
         <div className={toggleState === 1 ? "project__modal active-modal" : "project__modal"}>
             <div className="project__modal-content">
                 <i onClick={() => toggleTab(0)} className="uil uil-times project__modal-close"></i>
@@ -139,6 +139,114 @@ const ProjectItems = ({item}) => {
                 <h3 className="project__modal-title">{item.title}</h3>
                 <p className="project__modal-description">
                     Point of Sales System (Cashier) using Java Netbeans and MySQL
+                </p>
+
+                <Swiper loop={true} grabCursor={true} spaceBetween={24} pagination={{ clickable: true, }} breakpoints={{
+                    576: {
+                      slidesPerView: 1,
+                    },
+                    768: {
+                      slidesPerView: 1,
+                      spaceBetween: 48,
+                    },
+                  }} modules={[Pagination]}>
+                    
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more1} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more2} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more3} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more4} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more5} alt="" className="project__img" />
+                    </SwiperSlide>
+
+                </Swiper>
+            </div>
+        </div>     
+
+        <div className={toggleState === 5 ? "project__modal active-modal" : "project__modal"}>
+            <div className="project__modal-content">
+                <i onClick={() => toggleTab(0)} className="uil uil-times project__modal-close"></i>
+                <h3 className="project__modal-title">{item.title}</h3>
+                <p className="project__modal-description">
+                    Tic Tac Toe using PHP HTML with Minimax
+                </p>
+
+                <Swiper loop={true} grabCursor={true} spaceBetween={24} pagination={{ clickable: true, }} breakpoints={{
+                    576: {
+                      slidesPerView: 1,
+                    },
+                    768: {
+                      slidesPerView: 1,
+                      spaceBetween: 48,
+                    },
+                  }} modules={[Pagination]}>
+                    
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more1} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more2} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more3} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more4} alt="" className="project__img" />
+                    </SwiperSlide>
+
+                </Swiper>
+            </div>
+        </div>    
+
+        <div className={toggleState === 6 ? "project__modal active-modal" : "project__modal"}>
+            <div className="project__modal-content">
+                <i onClick={() => toggleTab(0)} className="uil uil-times project__modal-close"></i>
+                <h3 className="project__modal-title">{item.title}</h3>
+                <p className="project__modal-description">
+                    Ping Pong 2D
+                </p>
+
+                <Swiper loop={true} grabCursor={true} spaceBetween={24} pagination={{ clickable: true, }} breakpoints={{
+                    576: {
+                      slidesPerView: 1,
+                    },
+                    768: {
+                      slidesPerView: 1,
+                      spaceBetween: 48,
+                    },
+                  }} modules={[Pagination]}>
+                    
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more1} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more2} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more3} alt="" className="project__img" />
+                    </SwiperSlide>
+                    <SwiperSlide className="modal__card">
+                      <img src={item.more4} alt="" className="project__img" />
+                    </SwiperSlide>
+
+                </Swiper>
+            </div>
+        </div>  
+
+        <div className={toggleState === 7 ? "project__modal active-modal" : "project__modal"}>
+            <div className="project__modal-content">
+                <i onClick={() => toggleTab(0)} className="uil uil-times project__modal-close"></i>
+                <h3 className="project__modal-title">{item.title}</h3>
+                <p className="project__modal-description">
+                    Ping Pong 2D
                 </p>
 
                 <Swiper loop={true} grabCursor={true} spaceBetween={24} pagination={{ clickable: true, }} breakpoints={{
